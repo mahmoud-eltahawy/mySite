3e4abddacf46932f37e7b2811bebc8fb5b9a404e
mahmoud-eltahawy <gemmy9300@gmail.com>
2023-12-31 00:25:35 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1 class="bg-blue-400">welcom to eltahawy site</h1>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{ts,tsx,md,json}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
